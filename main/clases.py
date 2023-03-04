99b2c5fe44cf08711e7610c164982214f35c3b32
Михаил <misga.yats@gmail.com>
2023-03-04 13:09:55 +0100

    Phone class added, 4 homework done

--- main/clases.py
+++ main/clases.py
@@ -5,14 +5,15 @@ class Item:
     all = []
     pay_rate = 1
 
-    def __init__(self, name, price, ammount):
+    def __init__(self, name, price, ammount, sims=None):
         self.__name = name
         self.price = price
         self.ammount = ammount
         self.all.append(self)
+        self.number_of_sim = sims
 
     def __repr__(self):
-        return f"{self.__class__.__name__}('{self.first}', '{self.last}', {self.pay})"
+        return f"{self.__class__.__name__}('{self.__name}', '{self.price}', {self.ammount})"
 
     def __str__(self):
         return self.__name
@@ -49,3 +50,18 @@ class Item:
             self.__name = name
         else:
             raise Exception("Длина наименования товара превышает 10 символов.")
+
+
+class Phone(Item):
+
+    def __gt__(self, other):
+        if isinstance(other, Item):
+            return self.ammount + other.ammount
+
+    @number_of_sims.setter
+    def number_of_sims(self, sims):
+        """Проверяет длинну наименования товара"""
+        if sims > 0 and sims is not float:
+            self.number_of_sim = sims
+        else:
+            raise ValueError("Количество физических SIM-карт должно быть целым числом больше нуля.")
