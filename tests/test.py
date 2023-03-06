1fbf6d81b79b79377b8b8d5b4fbaa9195e5a272b
Михаил <misga.yats@gmail.com>
2023-03-06 12:42:39 +0100

    tests added

--- tests/test.py
+++ tests/test.py
@@ -1,16 +1,67 @@
-from main.clases import Item
+import pytest
+from main.clases import Item, Phone
 
 
-def test_item():
-    item1 = Item("Смартфон", 10000, 20)
-    item2 = Item("Ноутбук", 20000, 5)
-    assert item1.calculate_total_price() == 200000
-    assert item2.calculate_total_price() == 100000
-    Item.pay_rate = 0.8  # устанавливаем новый уровень цен
-    item1.apply_discount()
-    assert item1.price == 8000.0
-    assert item2.price == 20000
-    item2.apply_discount()
-    assert item2.price == 16000.0
-    assert len(Item.all) == 2
+@pytest.fixture
+def item():
+    return Item("Смартфон", 10000, 20)
+
+
+@pytest.fixture
+def phone():
+    return Phone("iPhone 14", 120_000, 5, 2)
+
+
+def test_electronic_store_init(item):
+    assert item.name == "Смартфон"
+    assert item.price == 10000
+    assert item.ammount == 20
+    assert item.pay_rate == 1
+
+
+def test_calculate_total_price(item):
+    assert item.calculate_total_price() == 200000
+
+
+def test_discount_price(item):
+    assert item.apply_discount() == 10000
+
+
+def test_is_integer(item):
+    assert Item.is_integer(5) is True
+    assert Item.is_integer(5.0) is True
+    assert Item.is_integer(5.5) is False
+
+
+def test_repr(item):
+    assert item.__repr__() == 'Item(Смартфон, 10000, 20)'
+
+
+def test_str(item):
+    assert item.__str__() == 'Смартфон'
+
+
+def test_add(item):
+    phone = Phone("iPhone 14", 120_000, 5, 2)
+    assert phone + item == 25
+
+
+def test_phone_init(phone):
+    assert phone.name == "iPhone 14"
+    assert phone.price == 120000
+    assert phone.ammount == 5
+    assert phone.number_of_sim == 2
+
+
+def test_phone_number_of_sim(phone):
+    phone.number_of_sim = 3
+    assert phone.number_of_sim == 3
+    with pytest.raises(ValueError):
+        phone.number_of_sim = 0
+
+
+def test_phone_repr(phone):
+    assert repr(phone) == 'Phone(iPhone 14, 120000, 5, 2)'
+
+
 
